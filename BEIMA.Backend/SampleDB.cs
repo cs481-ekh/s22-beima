cd7fb35a5b20a4f838b9a609f08d4826926a184b
Kenny-Miller <kennymilarhol@gmail.com>
2022-02-07 09:40:47 -0700

    Added sample db call to hit cloud db

---
+++ BEIMA.Backend/SampleDB.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using MongoDB.Driver;
+
+namespace BEIMA.Backend
+{
+    public static class SampleDB
+    {
+        [FunctionName("SampleDB")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            var connectionString = Environment.GetEnvironmentVariable("AzureCosmosConnection");
+            var dbClient = new MongoClient(connectionString);
+            var dbList = dbClient.ListDatabaseNames().ToList();
+
+            var json = JsonSerializer.Serialize(dbList);
+
+
+            return new OkObjectResult(json);
+        }
+    }
+}
