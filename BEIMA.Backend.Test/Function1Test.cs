ba387d99c482eb8a98fda0795bf39e3485754f04
Joseph Moore <josephmoore@u.boisestate.edu>
2022-02-09 21:55:47 -0700

    Added unit testing

---
+++ BEIMA.Backend.Test/Function1Test.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+
+namespace BEIMA.Backend.Test
+{
+    [TestFixture]
+    public class Function1Test
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void InitialState_Action_ExpectedResult()
+        {
+            Assert.Pass();
+            Assert.IsTrue(true);
+        }
+    }
+}
