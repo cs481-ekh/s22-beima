0d6cce4f2e8cf0ba6856c2d507526f951d24a349
Joseph Moore <43118853+josephmoore99@users.noreply.github.com>
2022-02-09 22:40:06 -0700

    Closes #28 (#44)

---
+++ BEIMA.Backend.Test/SampleFunctionTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace BEIMA.Backend.Test
+{
+    [TestFixture]
+    public class SampleFunctionTest
+    {
+        [Test]
+        public void InitialState_Action_ExpectedResult()
+        {
+            Assert.That(23, Is.Not.Null);
+        }
+    }
+}
