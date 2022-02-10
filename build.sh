6323bff14d41b747148a6a06b24df44e29b5bfea
AshlynAdamson <58532471+AshlynAdamson@users.noreply.github.com>
2022-02-09 21:44:17 -0700

    adding commands to build and test scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-exit 127
+
+#Update the apt package manager
+sudo apt update || {echo "Updating apt failed" exit 1}
+# Install NodeJS
+sudo apt install nodejs || {echo "Installing NodeJS failed" exit 1}
+# Install npm
+sudo apt install npm || {echo "Installing npm failed" exit 1}
+# Change directory to BEIMA.Client
+cd /BEIMA.Client || {echo "Changing directories failed" exit 1}
+# Install node_modules
+npm install react-scripts || {echo "Installing node_modules failed" exit 1}
+# Install version 0.6.5 of jest-watch-typeahead
+npm add --exact jest-watch-typeahead@0.6.5 || {echo "Installing version 0.6.5 of jest-watch-typeahead failed" exit 1}
