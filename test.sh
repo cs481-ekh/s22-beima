07dd3d2a77f52c59a3fa1956d1137f85b731de2f
Joseph Moore <josephmoore@u.boisestate.edu>
2022-02-10 15:55:40 -0700

    Add unit tests to test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Run unit tests
+dotnet test ./BEIMA.Backend.Test/BEIMA.Backend.Test.csproj || { echo "Running Unit tests failed"; exit 1; }
 # Change directories to BEIMA.Client
 cd ./BEIMA.Client || { echo "Changing directories failed"; exit 1; }
 # Run react tests
