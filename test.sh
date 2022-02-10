b542e3f06dca02c5a12f6a9bf31085b2a6113358
AshlynAdamson <58532471+AshlynAdamson@users.noreply.github.com>
2022-02-09 22:24:49 -0700

    adding spaces

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Change directories to BEIMA.Client
-cd /BEIMA.Client || {echo "Changing directories failed"; exit 1;}
+cd /BEIMA.Client || { echo "Changing directories failed"; exit 1; }
 # Run react tests
 npm test
