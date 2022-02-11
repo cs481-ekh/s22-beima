6932e4baa4d7bd10c86469cf5edf07c3a632c1e4
Joseph Moore <josephmoore@u.boisestate.edu>
2022-02-10 22:41:02 -0700

    maybe don't try to start

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ dotnet test ./BEIMA.Backend.Test.csproj || { echo "Unit tests failed"; exit 1; }
 # Switch to FT directory
 cd ../BEIMA.Backend.FT
 # Run backend FTs
-start-test 3000 || { echo "Failed to start application."; exit 1; }
+#start-test 3000 || { echo "Failed to start application."; exit 1; }
 dotnet test ./BEIMA.Backend.FT.csproj || { echo "FT tests failed"; exit 1; }
 # Go back to root directory
 cd ../
