ebc1c4565f7a88a8b77041dfe19bf137897ba8ab
Joseph Moore <josephmoore@u.boisestate.edu>
2022-02-10 23:45:43 -0700

    run after frontend tests

--- test.sh
+++ test.sh
@@ -4,10 +4,8 @@ cd ./BEIMA.Backend.Test
 # Run backend unit tests
 dotnet test ./BEIMA.Backend.Test.csproj || { echo "Unit tests failed"; exit 1; }
 # Switch to FT directory
-cd ../BEIMA.Backend.FT
-# Run backend FTs
-npm run build
-dotnet test ./BEIMA.Backend.FT.csproj || { echo "FT tests failed"; exit 1; }
+cd ../
+cd ./BEIMA.Backend.FT
 # Go back to root directory
 cd ../
 # Change directories to BEIMA.Client
@@ -15,3 +13,5 @@ cd ./BEIMA.Client || { echo "Changing directories failed"; exit 1; }
 # Run react tests
 npm test
 npm run cypress
+# Run backend FTs
+dotnet test ./BEIMA.Backend.FT.csproj || { echo "FT tests failed"; exit 1; }
