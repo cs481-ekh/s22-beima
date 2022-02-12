0745d63c15ede53486c7d64665031b75c9afb9a2
Joseph Moore <josephmoore@u.boisestate.edu>
2022-02-11 20:26:24 -0700

    directory stuff

--- test.sh
+++ test.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-run_backend_locally(){
-    echo "STARTING LOCAL API"
-    func start
-}
-
 # Change directory to BEIMA.Backend.Test
 cd ./BEIMA.Backend.Test
 # Run backend unit tests
 dotnet test ./BEIMA.Backend.Test.csproj || { echo "Unit tests failed"; exit 1; }
+cd ../
+cd ./BEIMA.Backend
+func start &
 # Switch to FT directory
 cd ../
 cd ./BEIMA.Backend.FT
 # Run backend FTs
-run_backend_locally &
 sleep 15
 dotnet test ./BEIMA.Backend.FT.csproj || { echo "FT tests failed"; exit 1; }
 # Go back to root directory
