f0654de7d012e6165f7bbe5333513387b5ef6ede
roguedeleter <47615072+roguedeleter@users.noreply.github.com>
2022-02-13 15:32:45 -0700

    Implemented some basic unit tests for MongoConnector, #60

---
+++ BEIMA.Backend.Test/MongoConnectorTest.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using BEIMA.Backend.MongoService;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+using MongoDB.Bson;
+
+namespace BEIMA.Backend.Test
+{
+    [TestFixture]
+    public class MongoConnectorTest
+    {
+        //private MongoConnector? mongo;
+        class LocalSettings
+        {
+            public bool IsEncrypted { get; set; }
+            public Dictionary<string, string> Values { get; set; }
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            string basePath = Path.GetFullPath(@"..\..\..\..\BEIMA.Backend");
+            var settings = JsonConvert.DeserializeObject<LocalSettings>(
+                File.ReadAllText(basePath + "\\local.settings.json"));
+
+            foreach (var setting in settings.Values)
+            {
+                Environment.SetEnvironmentVariable(setting.Key, setting.Value);
+            }
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void InitialState_Action_ExpectedResult()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void ConnectorNotCreated_CallConstructorFiveTimes_FiveInstancesAreEqual()
+        {
+            var connector1 = MongoConnector.Instance;
+            var connector2 = MongoConnector.Instance;
+            var connector3 = MongoConnector.Instance;
+            var connector4 = MongoConnector.Instance;
+            var connector5 = MongoConnector.Instance;
+            Assert.IsTrue(connector1 == connector2);
+            Assert.IsTrue(connector1 == connector3);
+            Assert.IsTrue(connector1 == connector4);
+            Assert.IsTrue(connector1 == connector5);
+        }
+
+        [Test]
+        public void ConnectorCreated_GetDeviceGivenDeviceId_DeviceDocumentReturned()
+        {
+            var mongo = MongoConnector.Instance;
+            var doc = mongo.GetDevice("61f4882f42dab933544849d3");
+            Assert.IsNotNull(doc);
+            Assert.IsTrue(doc is BsonDocument);
+        }
+
+        [Test]
+        public void ConnectorNotCreated_CallConstructorOnce_InstanceConnectedToCorrectEnvironment()
+        {
+            var mongo = MongoConnector.Instance;
+            if (Environment.GetEnvironmentVariable("CurrentEnv") == "dev-local")
+            {
+                Assert.IsTrue(mongo.CurrentServerType == ServerType.Local);
+            }
+            else
+            {
+                Assert.IsTrue(mongo.CurrentServerType == ServerType.Cloud);
+            }
+        }
+
+        [Test]
+        public void DocumentNotInserted_InsertDocument_DocumentHasBeenInserted()
+        {
+            var mongo = MongoConnector.Instance;
+            BsonDocument doc = new BsonDocument { 
+                { "deviceTypeId", "a" },
+                { "serialNumber", "a12345"}
+            };
+            var result = mongo.InsertDevice(doc);
+            Assert.IsTrue(result);
+        }
+
+
+
+
+
+    }
+}
