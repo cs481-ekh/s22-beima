d1d8c33963538b27fde8477119aed11269efc8f7
AshlynAdamson <58532471+AshlynAdamson@users.noreply.github.com>
2022-02-19 18:47:17 -0700

    MAIL

--- BEIMA.Client/src/shared/NavBar.js
+++ BEIMA.Client/src/shared/NavBar.js
@@ -1,33 +1,36 @@
 import logo from './BSU-logo.png';
-import { Row, Col} from 'react-bootstrap';
+import { Row, Col, Container} from 'react-bootstrap';
 import './shared.css';
 
 const NavBar = () => {
     return (
         <div className="sharedNavBar">
-            <Col>
-                <Row className="logo"><img src={logo} className="bsu-logo" alt="bsu-logo" /></Row>
-                <Row><div className="sharedHeader">BEIMA</div></Row>
-                <Row>
-                    <Col className="pageLinks">
-                        <Row className="pageLink">
-                            <a href="Devices" className="sharedText">Devices</a>
-                        </Row>
-                        <Row className="pageLink">
-                            <a href="Device Types" className="sharedText">Device Types</a>
-                        </Row> 
-                        <Row className="pageLink">
-                            <a href="Add Device" className="sharedText">Add Device {"\xa0\xa0"}+</a>
-                        </Row> 
-                        <Row className="pageLink">
-                            <a href="Add Device Types" className="sharedText">Add Device Type {"\xa0\xa0\xa0\xa0"}+</a>
-                        </Row>
-                        <Row className="pageLink">
-                            <a href="Help" className="sharedText">Help {"\xa0\xa0"}?</a>
-                        </Row>      
-                    </Col> 
-                </Row>
-            </Col>
+            <Container>
+              <Col>
+                    <Row className="logo"><img src={logo} className="bsu-logo" alt="bsu-logo" /></Row>
+                    <Row><div className="sharedHeader">BEIMA</div></Row>
+                    <Row>
+                        <Col className="pageLinks">
+                            <Row className="pageLink">
+                                <a href="Devices" className="sharedText">Devices</a>
+                            </Row>
+                            <Row className="pageLink">
+                                <a href="Device Types" className="sharedText">Device Types</a>
+                            </Row> 
+                            <Row className="pageLink">
+                                <a href="Add Device" className="sharedText">+ Add Device</a>
+                            </Row> 
+                            <Row className="pageLink">
+                                <a href="Add Device Types" className="sharedText">+ Add Device Type</a>
+                            </Row>
+                            <Row className="pageLink">
+                                <a href="Help" className="sharedText">? Help</a>
+                            </Row>      
+                        </Col> 
+                    </Row>
+                </Col>  
+            </Container>
+            
         </div>
     );
 }
