69ece21eacd23f13253ae8a9e3b09b66be334c6d
roguedeleter <47615072+roguedeleter@users.noreply.github.com>
2022-02-21 20:06:06 -0700

    Implemented BaseDevice class

---
+++ BEIMA.Backend.Test/MongoService/BaseDeviceTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MongoDB.Bson;
+using NUnit.Framework;
+
+namespace BEIMA.Backend.MongoService.Test
+{
+    [TestFixture]
+    public class BaseDeviceTest
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void InitialState_Action_ExpectedResult()
+        {
+            var objId = new ObjectId("61f4882f42dab933544849d3");
+            var device = new BaseDevice(objId, "asd", "asd", "xcsd", "cvliokj", "asd", 1, "sadloifk");
+            device.AddField("asdfwefsdf", "test-value");
+            device.AddField("abc", "test2");
+            Console.WriteLine(device.GetBsonDocument().ToString());
+        }
+    }
+}
---
+++ BEIMA.Backend/MongoService/BaseDevice.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BEIMA.Backend.MongoService
+{
+    public class BaseDevice
+    {
+
+        public BaseDevice()
+        {
+            if(Location == null)
+            {
+                Location = new BsonDocument();
+            }
+            if(Fields == null)
+            {
+                Fields = new BsonDocument();
+            }
+        }
+
+        public BaseDevice(ObjectId id, string deviceTypeId, string deviceTag, string manufacturer, string modelNum, string serialNum, int yearManufactured, string notes)
+        {
+            BsonClassMap.RegisterClassMap<BaseDevice>();
+
+            Id = id;
+            DeviceTypeId = deviceTypeId;
+            DeviceTag = deviceTag;
+            Manufacturer = manufacturer;
+            ModelNum = modelNum;
+            SerialNum = serialNum;
+            YearManufactured = yearManufactured;
+            Notes = notes;
+            Fields = new BsonDocument();
+            Location = new BsonDocument();
+            LastModified = new BsonDocument();
+        }
+
+
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("deviceTypeId")]
+        public string DeviceTypeId { get; set; }
+
+        [BsonElement("deviceTag")]
+        public string DeviceTag { get; set; }
+
+        [BsonElement("manufacturer")]
+        public string Manufacturer { get; set; }
+
+        [BsonElement("modelNum")]
+        public string ModelNum { get; set; }
+
+        [BsonElement("serialNum")]
+        public string SerialNum { get; set; }
+
+        [BsonElement("yearManufactured")]
+        public int YearManufactured { get; set; }
+
+        [BsonElement("notes")]
+        public string Notes { get; set; }
+
+        [BsonElement("fields")]
+        public BsonDocument Fields { get; set; }
+
+        [BsonElement("location")]
+        public BsonDocument Location { get; set; }
+
+        [BsonElement("lastModified")]
+        public BsonDocument LastModified { get; set; }
+
+        public BsonDocument GetBsonDocument()
+        {
+            return this.ToBsonDocument();
+        }
+
+        public void AddField(string key, dynamic value)
+        {
+            Fields.Add(key, value);
+        }
+
+        public void SetLocation(string buildingId, string notes, string latitude, string longitude)
+        {
+            //Check if null, if they are, then use empty string
+            buildingId ??= string.Empty;
+            notes ??= string.Empty;
+            latitude ??= string.Empty;
+            longitude ??= string.Empty;
+
+            Location.Add("buildingId", buildingId);
+            Location.Add("notes", notes);
+            Location.Add("latitude", latitude);
+            Location.Add("longitude", longitude);
+        }
+
+        public void SetLastModified(DateTime date, string user)
+        {
+            LastModified.Add("date", date);
+            LastModified.Add("user", user);
+        }
+        
+    }
+}
