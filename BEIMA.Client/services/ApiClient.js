aa6a6f69a411667aaa3d7cdf76643a92d26e54bf
unknown <thomashess@u.boisestate.edu>
2022-02-24 19:59:17 -0700

    updated spacing/file documentation

--- BEIMA.Client/services/ApiClient.js
+++ BEIMA.Client/services/ApiClient.js
@@ -2,7 +2,7 @@
 require('dotenv').config({ path: '../.env' })
 
 //axios package includes it's own try catch and error output functions 
-//and is declared globally so each request doesn't create its own connection
+
 const axios = require('axios');
 
 const API_URL = process.env.REACT_APP_API_URL;
@@ -11,16 +11,18 @@ const API_URL = process.env.REACT_APP_API_URL;
 /// Gets a device with the specified ID
 /// </summary>
 /// <param name="deviceId">The device ID to retrieve from the database</param>
-/// <returns>The device with the specified ID from the DB API as a JSON OBJECT, or the error message from the API</returns>
+/// <returns>JSON object containing the HTTP status code and the API response</returns>
 const GetDevice = async (deviceId) => {
   //performs the get and returns the data or error
   const response = await axios.get(API_URL + "device/" + deviceId).then(response => { 
-    return JSON.parse('{ "status" : "' + response.status + '", "response" : ' + JSON.stringify(response.data) + '}');
-  }).catch(function (error) {
-      if (error.response) {
-      return JSON.parse('{ "status" : "' + error.response.status + '", "response" : ' + JSON.stringify(error.response.data) + '}');
-    }
+      let stringdata = JSON.stringify(response.data);
+      return JSON.parse('{ "status" : "' + response.status + '", "response" : ' + JSON.stringify(response.data) + '}');
+    }).catch(function (error) {
+        if (error.response) {
+        return JSON.parse('{ "status" : "' + error.response.status + '", "response" : ' + JSON.stringify(error.response.data) + '}');
+      }
   });
+  
   return response;
 }
 
