3e5372a9aa8f5925aa6b5a8b703ee069ce72365c
AshlynAdamson <58532471+AshlynAdamson@users.noreply.github.com>
2022-02-25 21:28:53 -0700

    add device page close, modularizing items where I can

---
+++ BEIMA.Client/src/shared/FormList/FormList.js
@@ -0,0 +1,16 @@
+import { Form } from 'react-bootstrap';
+
+const FormList = ({fields}) => {
+    return (
+      <div>
+        {fields.map(element =>
+          <Form.Group>
+            <Form.Label>{element}</Form.Label>
+            <Form.Control type="text" name={element} placeholder={"Enter " + element}/>
+          </Form.Group>
+        )} 
+      </div>
+    )
+}
+
+export default FormList
