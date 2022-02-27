f6f9fbc77ed6b2e2a6ac6ef6bd9dcc6c73c1156f
unknown <thomashess@u.boisestate.edu>
2022-02-27 10:21:58 -0700

    updated fucntion documetnation documetnation and removed Node module syntax

--- BEIMA.Client/services/ApiClient.js
+++ BEIMA.Client/services/ApiClient.js
@@ -1,28 +1,24 @@
-//pull in the vars file to get the API URL
-require('dotenv').config({ path: '../.env' })
-
-//axios package includes it's own try catch and error output functions 
-
-const axios = require('axios');
-
+import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
 
-/// <summary>
-/// Gets a device with the specified ID
-/// </summary>
-/// <param name="deviceId">The device ID to retrieve from the database</param>
-/// <returns>JSON object containing the HTTP status code and the API response</returns>
-const GetDevice = async (deviceId) => {
+/**
+ * Gets a JSON object from the DB with the provided device ID
+ *
+ * @param The device id in the database to retrieve
+ * @return JSON document from the DB wrapped in a JSON object with the HTTP response code
+ */
+export default async (deviceId) => {
   //performs the get and returns the data or error
-  const dbCall = await axios.get(API_URL + "device/" + deviceId);
+  const dbCall = await axios.get(API_URL + "device/" + deviceId).catch(function (error) {
+      if (error.response) {
+      return error.response;
+    }
+  });
+
   const response = {
     status: dbCall.status,
     response: dbCall.data
   }
+
   return response;
 }
-
-//module.exports.<Custom Name> = <Function Name>
-//OR
-//module.exports.<Function Name> = <Function Name>
-module.exports.GetDevice = GetDevice;
