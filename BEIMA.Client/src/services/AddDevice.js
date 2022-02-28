5fcdb7e336855afc0bc95c37c6f6a0ecb8a05f3c
unknown <thomashess@u.boisestate.edu>
2022-02-28 15:54:21 -0700

    correctd file

---
+++ BEIMA.Client/src/services/AddDevice.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const API_URL = process.env.REACT_APP_API_URL;
+
+/**
+ * POSTs a JSON object to the API for storage in DB
+ *
+ * @param The device details to add to the DB
+ * @return Error message or the inserted device ID
+ */
+export default async (deviceDetails) => {
+  //performs the post and returns an error message or the inserted device ID
+  const dbCall = await axios.post(API_URL + "device/", deviceDetails).catch(function (error) {
+      if (error.response) {
+        return error.response;
+    }
+  });
+console.log(dbCall);
+  const response = {
+    status: dbCall.status,
+    response: dbCall.data
+  }
+
+  return response;
+}
