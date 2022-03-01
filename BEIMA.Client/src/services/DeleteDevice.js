c68a140ce1ed9d5a887a7735a631b8743676cc56
thessbsu <46760776+thessbsu@users.noreply.github.com>
2022-02-28 19:49:24 -0700

    added function name

--- BEIMA.Client/src/services/DeleteDevice.js
+++ BEIMA.Client/src/services/DeleteDevice.js
@@ -7,7 +7,7 @@ const API_URL = process.env.REACT_APP_API_URL;
  * @param The ID of the device to delete
  * @return Error message or a succes indicator
  */
-export default async (deviceId) => {
+export default async deleteDevice (deviceId) => {
   //performs the post and returns an error message or a succes indicator
   const dbCall = await axios.post(API_URL + "device/" + deviceId + "/delete").catch(function (error) {
       if (error.response) {
