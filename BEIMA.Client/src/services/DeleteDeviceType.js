385c8e1f48c6f9ca388b6d7c63000d094ab43c5b
thessbsu <46760776+thessbsu@users.noreply.github.com>
2022-03-06 15:46:23 -0700

    fixed spelling/grammar in function header, fixed exported function name

--- BEIMA.Client/src/services/DeleteDeviceType.js
+++ BEIMA.Client/src/services/DeleteDeviceType.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
 
 /**
- * POSTs an Devite Type ID for the API to delete
+ * POSTs a Device Type ID for the API to delete
  *
  * @param The ID of the device type to delete
  * @return Error message or a succes indicator
  */
-export default async function deleteDevice(deviceTypeId) {
+export default async function deleteDeviceType(deviceTypeId) {
   //performs the post and returns an error message or a succes indicator
   const dbCall = await axios.post(API_URL + "device-type/" + deviceTypeId + "/delete").catch(function (error) {
       if (error.response) {
