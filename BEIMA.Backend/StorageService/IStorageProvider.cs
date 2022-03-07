47fa85654a1b41b5837011112fba51083be1e9f6
Kenny-Miller <kennymilarhol@gmail.com>
2022-03-06 18:36:48 -0700

    Added comments

--- BEIMA.Backend/StorageService/IStorageProvider.cs
+++ BEIMA.Backend/StorageService/IStorageProvider.cs
@@ -9,17 +9,36 @@ using System.Threading.Tasks;
 namespace BEIMA.Backend.StorageService
 {
     /// <summary>
-    /// This class abstracts basic CRUD operations for MongoDB. It is implemented as a 
-    /// singleton class, so to instantiate, use this syntax: "var mongo = MongoConnector.Instance;".
+    /// This class abstracts basic file storage operations. It is implemented as a 
+    /// singleton dependancy injected object. See SampleFileStore.cs for example
+    /// of how to inject it through the function's constructor
     /// </summary>
     public interface IStorageProvider
     {
-        /**
-         * 
-         */
+        /// <summary>
+        /// Puts a file into the the storage bucket
+        /// </summary>
+        /// <param name="file">Corresponds to a file sent as part of a multipart/form-data post</param>
+        /// <returns>Uid of the file created or null if request failed</returns>
         Task<string> PutFile(IFormFile file);
+        /// <summary>
+        /// Creates a presigned url for the specified file
+        /// </summary>
+        /// <param name="fileUid">Corresponds the target file's uid filename</param>
+        /// <returns>Presigned url or null if request failed</returns>
         Task<string> GetPresignedURL(string fileUid);
+        /// <summary>
+        /// Gets a stream to the requested file. Used for downloading a file. Will be
+        /// used in conjuction with return new FileStreamResult(stream, "application/octet-stream");
+        /// </summary>
+        /// <param name="fileUid">Corresponds the target file's uid filename</param>
+        /// <returns>Memorystream of the target file or null if request failed</returns>
         Task<MemoryStream> GetFileStream(string fileUid);
+        /// <summary>
+        /// Deletes the target file from storage
+        /// </summary>
+        /// <param name="fileUid">Corresponds the target file's uid filename</param>
+        /// <returns>True if target file was deleted or false if request failed</returns>
         Task<bool> DeleteFile(string fileUid);
     }
 }
