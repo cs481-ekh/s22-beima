00ca58ef60dbbf98d893840589df6203c32a8fb0
AshlynAdamson <58532471+AshlynAdamson@users.noreply.github.com>
2022-03-08 08:53:39 -0700

    getting device type list mostly done

---
+++ BEIMA.Client/src/services/GetDeviceTypeList.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+const API_URL = process.env.REACT_APP_API_URL;
+
+/**
+ * GET API call to get a list of device types in the database and return only basic fields
+ *
+ * @return JSON with list of all devices or error message on failure
+ */
+const GetDeviceTypeList = async() => {
+  const deviceTypeListCall = await axios.get(API_URL + "device-type-list").catch(function (error) {
+      if (error.response) {
+        return error.response;
+    }
+  });
+
+  let filteredFields = deviceTypeListCall.data.map((item) => {
+    return {
+      id: item._id,
+      name: item.name,
+      description: item.description,
+      notes: item.notes,
+      lastModified: item.lastModified.date
+    }
+  })
+
+  const response = {
+    status: deviceTypeListCall.status,
+    response: filteredFields
+  }
+
+  console.log(response);
+
+  return response;
+}
+
+export default GetDeviceTypeList
