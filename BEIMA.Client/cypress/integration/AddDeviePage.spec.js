281bbd7bd52d59e359a510beb2fe48eacf359cb0
thessbsu <46760776+thessbsu@users.noreply.github.com>
2022-03-09 16:56:03 -0700

    corrected lat long decimal spec in tests, had 2 decimal points

--- BEIMA.Client/cypress/integration/AddDeviePage.spec.js
+++ BEIMA.Client/cypress/integration/AddDeviePage.spec.js
@@ -16,7 +16,7 @@ describe("Verify Data can be entered into fields", () => {
   it('Enter data into Building, Latitude, and Serial Number fields', () => {
     cy.visit('http://localhost:3000/addDevice')
     cy.get('#inputBuilding').scrollIntoView().type("Student Union Building")
-    cy.get('#inputLatitude').scrollIntoView().type("10.3445.2345")
+    cy.get('#inputLatitude').scrollIntoView().type("10.34452345")
     cy.get("[id='inputSerial Number']").scrollIntoView().type("12345")
   })
 })
@@ -25,7 +25,7 @@ describe("Verify Data in fields is cleared when Add Device is selected", () => {
   it('Enter data, click Add Device, verify fields are empty', () => {
     cy.visit('http://localhost:3000/addDevice')
     cy.get('#inputBuilding').scrollIntoView().type("Student Union Building")
-    cy.get('#inputLatitude').scrollIntoView().type("10.3445.2345")
+    cy.get('#inputLatitude').scrollIntoView().type("10.34452345")
     cy.get("[id='inputSerial Number']").scrollIntoView().type("12345")
     cy.get("#addDevice").scrollIntoView().click()
     cy.get('#inputBuilding').should('have.value', '')
