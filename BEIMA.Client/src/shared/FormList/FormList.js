991ee6dce7adbd2a738f44d8cbd80dd3148b6e8a
thessbsu <46760776+thessbsu@users.noreply.github.com>
2022-03-09 19:09:54 -0700

    restored FormList.js, added FormListWithErrorFeedback.js

--- BEIMA.Client/src/shared/FormList/FormList.js
+++ BEIMA.Client/src/shared/FormList/FormList.js
@@ -1,13 +1,12 @@
 import { Form } from 'react-bootstrap';
 
-const FormList = ({fields, errors}) => {
+const FormList = ({fields}) => {
     return (
       <div>
         {fields.map(element =>
           <Form.Group key={element} id={element}>
             <Form.Label>{element}</Form.Label>
-            <Form.Control id={"input" + element} type="text" name={element} placeholder={"Enter " + element} isInvalid={errors[element]}/>
-              <Form.Control.Feedback type='invalid'> { errors[element] }</Form.Control.Feedback>
+            <Form.Control id={"input" + element} type="text" name={element} placeholder={"Enter " + element}/>
           </Form.Group>
         )} 
       </div>
