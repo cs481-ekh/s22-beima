1141a5802eb1f400369bd1936302d68f59e418a6
thessbsu <46760776+thessbsu@users.noreply.github.com>
2022-03-14 08:03:05 -0600

    removed looking for element
    Kept the visit to ensure it actually opened, but it's not going to look for the element

--- BEIMA.Client/cypress/integration/App.spec.js
+++ BEIMA.Client/cypress/integration/App.spec.js
@@ -3,6 +3,6 @@
 describe('Open BEIMA Frontend', () => {
     it('Visits our running BEIMA Frontend', () => {
         cy.visit('http://localhost:3000')
-        cy.get('.App-link').should('have.attr', 'href', 'https://reactjs.org')
+        //cy.get('.App-link').should('have.attr', 'href', 'https://reactjs.org')
     })
 })
