681f1b9d087962fb3010ed8abfba2372567d1830
Joseph Moore <josephmoore@u.boisestate.edu>
2022-03-20 15:04:49 -0600

    unit tests

---
+++ BEIMA.Backend.Test/BuildingFunctions/GetBuildingTest.cs
@@ -0,0 +1,117 @@
+﻿using BEIMA.Backend.BuildingFunctions;
+using BEIMA.Backend.MongoService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Net;
+using static BEIMA.Backend.Test.RequestFactory;
+
+namespace BEIMA.Backend.Test.BuildingFunctions
+{
+    [TestFixture]
+    public class GetBuildingTest : UnitTestBase
+    {
+        #region FailureTests
+
+        [Test]
+        public void IdNotInDatabase_GetBuilding_ReturnsNotFound()
+        {
+            // ARRANGE
+
+            // Setup mock database client.
+            Mock<IMongoConnector> mockDb = new Mock<IMongoConnector>();
+            var testId = "1234567890abcdef12345678";
+            mockDb.Setup(mock => mock.GetBuilding(It.Is<ObjectId>(oid => oid == new ObjectId(testId))))
+                  .Returns<BsonDocument>(null)
+                  .Verifiable();
+            MongoDefinition.MongoInstance = mockDb.Object;
+
+            // Set up the http request.
+            var request = CreateHttpRequest(RequestMethod.GET);
+            var logger = (new LoggerFactory()).CreateLogger("Testing");
+
+            // ACT
+            var response = GetBuilding.Run(request, testId, logger);
+
+            // ASSERT
+            Assert.DoesNotThrow(() => mockDb.Verify(mock => mock.GetBuilding(It.IsAny<ObjectId>()), Times.Once));
+
+            Assert.That(response, Is.TypeOf(typeof(NotFoundObjectResult)));
+            Assert.That(((NotFoundObjectResult)response).StatusCode, Is.EqualTo((int)HttpStatusCode.NotFound));
+            Assert.That(((NotFoundObjectResult)response).Value, Is.EqualTo("Building could not be found."));
+        }
+
+        [TestCase("")]
+        [TestCase("xxx")]
+        [TestCase("123")]
+        [TestCase("10alskdjfhgytur74839skcm")]
+        public void InvalidId_GetBuilding_ReturnsInvalidId(string id)
+        {
+            // ARRANGE
+            Mock<IMongoConnector> mockDb = new Mock<IMongoConnector>();
+            mockDb.Setup(mock => mock.GetBuilding(It.Is<ObjectId>(oid => oid == new ObjectId(id))))
+                  .Returns<BsonDocument>(null)
+                  .Verifiable();
+            MongoDefinition.MongoInstance = mockDb.Object;
+
+            var request = CreateHttpRequest(RequestMethod.GET);
+            var logger = (new LoggerFactory()).CreateLogger("Testing");
+
+            // ACT
+            var response = GetBuilding.Run(request, id, logger);
+
+            // ASSERT
+            Assert.DoesNotThrow(() => mockDb.Verify(mock => mock.GetBuilding(It.IsAny<ObjectId>()), Times.Never));
+
+            Assert.That(response, Is.TypeOf(typeof(BadRequestObjectResult)));
+            Assert.That(((BadRequestObjectResult)response).StatusCode, Is.EqualTo((int)HttpStatusCode.BadRequest));
+            Assert.That(((BadRequestObjectResult)response).Value, Is.EqualTo("Invalid Id."));
+        }
+
+        #endregion FailureTests
+
+        #region SuccessTests
+
+        [Test]
+        public void IdIsValid_GetBuilding_ReturnsBuilding()
+        {
+            // ARRANGE
+
+            var testId = "1234567890abcdef12345678";
+
+            var dbBuilding = new Building(new ObjectId(testId), "Student Union", "1234", "Some Notes.");
+            dbBuilding.SetLastModified(DateTime.UtcNow, "Anonymous");
+            dbBuilding.SetLocation("1.234", "5.678");
+
+            Mock<IMongoConnector> mockDb = new Mock<IMongoConnector>();
+            mockDb.Setup(mock => mock.GetBuilding(It.Is<ObjectId>(oid => oid == new ObjectId(testId))))
+                  .Returns(dbBuilding.GetBsonDocument())
+                  .Verifiable();
+            MongoDefinition.MongoInstance = mockDb.Object;
+
+            var request = CreateHttpRequest(RequestMethod.GET);
+            var logger = (new LoggerFactory()).CreateLogger("Testing");
+
+            // ACT
+            var response = GetBuilding.Run(request, testId, logger);
+
+            // ASSERT
+            Assert.DoesNotThrow(() => mockDb.Verify(mock => mock.GetBuilding(It.IsAny<ObjectId>()), Times.Once));
+
+            Assert.That(response, Is.TypeOf(typeof(OkObjectResult)));
+            Assert.That(((OkObjectResult)response).StatusCode, Is.EqualTo((int)HttpStatusCode.OK));
+            var building = (Building)((OkObjectResult)response).Value;
+            Assert.That(building.Name, Is.EqualTo("Student Union"));
+            Assert.That(building.Number, Is.EqualTo("1234"));
+            Assert.That(building.Notes, Is.EqualTo("Some Notes."));
+            Assert.That(building.Location.Latitude, Is.EqualTo("1.234"));
+            Assert.That(building.Location.Longitude, Is.EqualTo("5.678"));
+        }
+
+        #endregion SuccessTests
+
+    }
+}
