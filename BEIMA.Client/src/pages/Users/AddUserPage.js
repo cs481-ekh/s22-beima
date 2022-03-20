6d8476be140f5514212892a832bbf5ab9795bf9a
thessbsu <46760776+thessbsu@users.noreply.github.com>
2022-03-20 17:33:27 -0600

    added password field and validation

--- BEIMA.Client/src/pages/Users/AddUserPage.js
+++ BEIMA.Client/src/pages/Users/AddUserPage.js
@@ -2,24 +2,59 @@ import styles from './AddUserPage.module.css';
 import { useOutletContext, Link } from 'react-router-dom';
 import { IoArrowBack } from "react-icons/io5";
 import { Card, Button, Form, Row, Col } from 'react-bootstrap';
-import FormList from '../../shared/FormList/FormList.js';
+import FormListWithErrorFeedback from '../../shared/FormList/FormListWithErrorFeedback.js';
 import { useEffect, useState } from "react";
 
 const AddUserPage = () => {
   const mandatoryUserFields = {
     "Username" : "",
+    "Password" : "",
+    "Password Confirmation" : "",
     "First Name" : "",
     "Last Name" : "",
     "Role" : ""
   }
   
-  const [userFields] = useState(mandatoryUserFields);
+  const [userFields, setUserFields] = useState(mandatoryUserFields);
   const [setPageName] = useOutletContext();
   const [fullUserJSON, setFullUserJSON] = useState({});
+  const [passwordErrors, setPasswordErrors] = useState({});
+  const [errors, setErrors] = useState({});
   
   useEffect(() => {
     setPageName('Add User')
   }, [setPageName])
+  
+  /*
+  * updates the values in state when the user types
+  * @param inputEvent the even fired when the user types
+  */
+  function updateFieldState(inputEvent){
+    userFields[inputEvent.target.name] = inputEvent.target.value;
+    setUserFields(userFields);
+  }
+  
+  function checkPassword(){
+    let passwordErrors = {};
+    
+    if(userFields["Password"] === '') {
+      passwordErrors["Password"] = 'Pasword cannot be empty';
+    }
+
+    if(userFields["Password Confirmation"] === '') {
+      passwordErrors["Password Confirmation"] = 'Pasword Confirmation cannot be empty';
+    }
+    
+    if((!('Password' in passwordErrors) && !('Password Confirmation' in passwordErrors)) &&
+       (userFields["Password"] !== userFields["Password Confirmation"])
+      ) {
+         passwordErrors["Password"] = 'Paswords do not match';
+         passwordErrors["Password Confirmation"] = 'Paswords do not match';
+    }
+    
+    return passwordErrors;
+  }
+  
   
   // gathers all the input and puts it into JSON
   function createJSON(addButtonEvent){
@@ -34,14 +69,29 @@ const AddUserPage = () => {
         let formJSON =  {[formName] : formFields[i].value};
         
         Object.assign(fieldValues, formJSON);
-        formFields[i].value = "";
       }
     }
-
-    setFullUserJSON(fieldValues);
     
-    // replace with user api calls when ready
-    console.log(fullUserJSON);
+    let newErrors = checkPassword();
+    
+    //display errors when present or attempt insert when valid data is present
+    if ( Object.keys(newErrors).length > 0 ) {
+      setErrors(newErrors);
+    } else {
+      setFullUserJSON(fieldValues);
+      
+      
+      // TODO replace with user api calls when ready
+      console.log(fullUserJSON);
+      
+      
+      
+      for(let i = 0; i < formFields.length; i++){
+        formFields[i].value = "";
+      }
+      setErrors({});
+      setUserFields(mandatoryUserFields);
+    }
   }
   
   return (
@@ -64,7 +114,7 @@ const AddUserPage = () => {
             <br/>
             <h4>User Fields</h4>
             <div>
-              <FormList fields={Object.keys(mandatoryUserFields)} />
+              <FormListWithErrorFeedback fields={Object.keys(userFields)} errors={errors} changeHandler={updateFieldState}/>
             </div>
          </Form>
         </Card.Body>
