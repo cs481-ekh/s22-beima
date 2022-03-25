d6e91dac569df10664665499667f885682821a52
Kenny-Miller <kennymilarhol@gmail.com>
2022-03-25 09:51:46 -0600

    wip

---
+++ BEIMA.Backend/ReportService/IReportService.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEIMA.Backend.ReportService
+{
+    public interface IReportService
+    {
+        public byte[] GeneratDeviceReportByDeviceType(ObjectId deviceType);
+
+        public byte[] GenerateAllDeviceReports();
+    }
+}
---
+++ BEIMA.Backend/ReportService/ReportService.cs
@@ -0,0 +1,53 @@
+﻿using BEIMA.Backend.MongoService;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEIMA.Backend.ReportService
+{
+    public class ReportService : IReportService
+    {
+        private readonly IMongoConnector db; 
+
+        private ReportService()
+        {
+            db = MongoDefinition.MongoInstance;
+        }
+
+        public byte[] GeneratDeviceReportByDeviceType(ObjectId deviceType)
+        {
+
+            return null;
+        }
+
+        public byte[] GenerateAllDeviceReports()
+        {
+            var deviceDocuments = db.GetAllDevices();
+
+            var typeToDevices = new Dictionary<ObjectId, List<Device>>();
+            foreach (var deviceDoc in deviceDocuments)
+            {
+                var device = BsonSerializer.Deserialize<Device>(deviceDoc);
+                if (typeToDevices.ContainsKey(device.DeviceTypeId))
+                {
+                    typeToDevices[device.DeviceTypeId].Add(device);
+                } else
+                {
+                    typeToDevices.Add(device.DeviceTypeId, new List<Device> { device });
+                }
+            }
+            
+            var deviceTypes = db.GetAllDeviceTypes();
+
+
+
+
+
+            return null;
+        }
+    }
+}
