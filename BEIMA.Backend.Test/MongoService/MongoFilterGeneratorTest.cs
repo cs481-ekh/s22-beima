9d7a69b400545ce404a271f6468157fd24b07acd
roguedeleter <47615072+roguedeleter@users.noreply.github.com>
2022-03-29 20:10:07 -0600

    Implement test for CombineFilters

--- BEIMA.Backend.Test/MongoService/MongoFilterGeneratorTest.cs
+++ BEIMA.Backend.Test/MongoService/MongoFilterGeneratorTest.cs
@@ -47,5 +47,25 @@ namespace BEIMA.Backend.Test.MongoService
             Assert.That(filter, Is.InstanceOf(typeof(FilterDefinition<BsonDocument>)));
             Assert.That(doc.GetElement(key).Value, Is.EqualTo(BsonNull.Value));
         }
+
+        [TestCase("key1", "value1", "key2", "value2")]
+        public void TwoFiltersGenerated_CombineFilters_FiltersCombined(string key1, dynamic value1, string key2, dynamic value2)
+        {
+            //Create filters
+            var f1 = MongoFilterGenerator.GetEqualsFilter(key1, value1);
+            var f2 = MongoFilterGenerator.GetEqualsFilter(key2, value2);
+            var combinedFilter = MongoFilterGenerator.CombineFilters(f1, f2);
+
+            //Render the filter as a BsonDocument so we can run tests to make sure the filter is correct
+            var serializerRegistry = BsonSerializer.SerializerRegistry;
+            var documentSerializer = serializerRegistry.GetSerializer<BsonDocument>();
+            var doc = combinedFilter.Render(documentSerializer, serializerRegistry);
+
+            //Ensure filter is correct
+            Assert.That(combinedFilter, Is.Not.Null);
+            Assert.That(combinedFilter, Is.InstanceOf(typeof(FilterDefinition<BsonDocument>)));
+            Assert.That(doc.GetElement(key1).Value?.ToString()?.ToLower(), Is.EqualTo(value1.ToString().ToLower()));
+            Assert.That(doc.GetElement(key2).Value?.ToString()?.ToLower(), Is.EqualTo(value2.ToString().ToLower()));
+        }
     }
 }
