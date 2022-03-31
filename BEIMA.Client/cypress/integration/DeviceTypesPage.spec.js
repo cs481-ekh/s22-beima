2d7d5fc5267c187fed89dcc051a052eca15b4be1
thessbsu <46760776+thessbsu@users.noreply.github.com>
2022-03-31 16:35:05 -0600

    changed to >=3 on the condition

--- BEIMA.Client/cypress/integration/DeviceTypesPage.spec.js
+++ BEIMA.Client/cypress/integration/DeviceTypesPage.spec.js
@@ -13,8 +13,8 @@ describe("Verify the list has items present", function () {
     cy.visit('http://localhost:3000/deviceTypes')
 
     //get all elements on the page with a class that starts with "ItemList_item"
-    //if there's at least one device in the DB it will pass since 2 elements with
+    //if there's at least one device in the DB it will pass since 3 elements with
     //the expected class are created for each device listing
-    cy.get('[class^="ItemList_item"]').its('length').should('be.gt', 2);
+    cy.get('[class^="ItemList_item"]').its('length').should('be.gte', 3);
   })
 })
