5af4f871830d8f286b40d094fca285bf24df181f
roguedeleter <47615072+roguedeleter@users.noreply.github.com>
2022-04-01 17:54:19 -0600

    Initial implementation of UserFT

--- BEIMA.Backend.FT/UserFT.cs
+++ BEIMA.Backend.FT/UserFT.cs
@@ -8,6 +8,29 @@ namespace BEIMA.Backend.FT
     [TestFixture]
     public class UserFT : FunctionalTestBase
     {
+        [TestCase("xxx")]
+        [TestCase("1234")]
+        [TestCase("1234567890abcdef1234567x")]
+        public void InvalidId_UserGet_ReturnsInvalidId(string id)
+        {
+            var ex = Assert.ThrowsAsync<BeimaException>(async () =>
+                await TestClient.GetUser(id)
+            );
+            Assert.IsNotNull(ex);
+            Assert.That(ex?.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+
+        [TestCase("")]
+        [TestCase("1234567890abcdef12345678")]
+        public void NoUsersInDatabase_UserGet_ReturnsNotFound(string id)
+        {
+            var ex = Assert.ThrowsAsync<BeimaException>(async () =>
+                await TestClient.GetUser(id)
+            );
+            Assert.IsNotNull(ex);
+            Assert.That(ex?.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+
         [Test]
         public async Task UserNotInDatabase_AddUser_CreatesNewUser()
         {
