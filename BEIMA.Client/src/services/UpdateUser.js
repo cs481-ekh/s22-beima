e526d0845adae2489f94b2f3e57f20139de417c8
AshlynAdamson <58532471+AshlynAdamson@users.noreply.github.com>
2022-04-02 17:41:51 -0600

    adding the rest of the services

---
+++ BEIMA.Client/src/services/UpdateUser.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const API_URL = process.env.REACT_APP_API_URL;
+
+/**
+ * POSTs a JSON object to the database to be updated
+ *
+ * @param The JSON of the user to be updated
+ * @return Error message or a success indicator
+ */
+export default async function updateUser(userDetails) {
+  //performs the post and returns an error message or a success indicator
+  const dbCall = await axios.post(API_URL + "device/" + userDetails._id + "/update", userDetails).catch(function (error) {
+      if (error.response) {
+        return error.response;
+    }
+  });
+
+  const response = {
+    status: dbCall.status,
+    response: dbCall.data
+  }
+
+  return response;
+}
