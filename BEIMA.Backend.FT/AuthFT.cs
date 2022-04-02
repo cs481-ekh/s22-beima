715ef9c274308e691924c93d2852d8d5479909ce
Kenny-Miller <kennymilarhol@gmail.com>
2022-04-02 14:25:20 -0600

    Added login endpoint

---
+++ BEIMA.Backend.FT/AuthFT.cs
@@ -0,0 +1,84 @@
+﻿using JWT.Builder;
+using NUnit.Framework;
+using System.Net;
+using System.Threading.Tasks;
+using static BEIMA.Backend.FT.TestObjects;
+
+namespace BEIMA.Backend.FT
+{
+    [TestFixture]
+    public class AuthFT : FunctionalTestBase
+    {
+        [SetUp]
+        public async Task SetUp()
+        {
+            // Add in deleting all the users in the database once functionality implemented
+        }
+
+        [TestCase(null, null)]
+        [TestCase("valid", null)]
+        [TestCase(null, "valid")]
+        public void InvalidParameters_Login_ReturnsNull(string username, string password)
+        {
+            var loginRequest = new LoginRequest()
+            {
+                Username = username,
+                Password = password
+            };
+
+            var ex = Assert.ThrowsAsync<BeimaException>(async () =>
+                await TestClient.Login(loginRequest)
+            );
+            Assert.IsNotNull(ex);
+            Assert.That(ex?.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [TestCase("notvalid", "notvalid")]
+        [TestCase("notvalid2", "notvalid2")]
+        public void ValidParametersNoUsers_Login_ReturnsNull(string username, string password)
+        {
+            var loginRequest = new LoginRequest()
+            {
+                Username = username,
+                Password = password
+            };
+
+            var ex = Assert.ThrowsAsync<BeimaException>(async () =>
+                await TestClient.Login(loginRequest)
+            );
+            Assert.IsNotNull(ex);
+            Assert.That(ex?.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [TestCase("valid", "valid")]
+        [TestCase("valid2", "valid2")]
+        public async Task ValidParametersWithUsers_Login_ReturnsJwtToken(string username, string password)
+        {
+            var user = new User()
+            {
+                Username = username,
+                Password = password,
+                Role = "admin"
+            };
+
+            var addResponse = await TestClient.AddUser(user);
+            Assume.That(addResponse, Is.Not.Null);
+
+            var loginRequest = new LoginRequest()
+            {
+                Username = username,
+                Password = password
+            };
+
+            var token = await TestClient.Login(loginRequest);
+            Assert.That(token, Is.Not.Null);
+
+            var claims = new JwtBuilder().Decode<Claims>(token);
+            Assert.That(claims.Username, Is.EqualTo(username));
+            Assert.That(claims.Role, Is.EqualTo("admin"));
+            Assert.That(claims.Exp, Is.Not.Null);
+            Assert.That(claims.Sub, Is.EqualTo("User"));
+            Assert.That(claims.Iss, Is.EqualTo("Beima"));
+        }
+    }
+}
--- BEIMA.Backend/AuthFunctions/Login.cs
+++ BEIMA.Backend/AuthFunctions/Login.cs
@@ -11,42 +11,74 @@ using BEIMA.Backend.Models;
 using BEIMA.Backend.AuthService;
 using BEIMA.Backend.MongoService;
 using MongoDB.Bson.Serialization;
+using BCryptNet = BCrypt.Net.BCrypt;
+using MongoDB.Bson;
+using System.Collections.Generic;
 
 namespace BEIMA.Backend.AuthFunctions
 {
+    /// <summary>
+    /// Handles a request to login
+    /// </summary>
     public static class Login
     {
+        /// <summary>
+        /// Handles a login request.
+        /// </summary>
+        /// <param name="req">The http request.</param>
+        /// <param name="log">The logger to log to.</param>
+        /// <returns>An http response containing a jwt token of the user</returns>
         [FunctionName("Login")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "login")] HttpRequest req,
             ILogger log)
         {
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var data = JsonConvert.DeserializeObject<LoginRequest>(requestBody);
+            List<BsonDocument> userDocs;
+            string password;
+            try
+            {
+                // Deserialize request
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var data = JsonConvert.DeserializeObject<LoginRequest>(requestBody);
+                
+                if(data == null || data.Username == null || data.Password == null)
+                {
+                    return new UnauthorizedResult();
+                }
+                password = data.Password;
 
-
-            var dbServce = MongoDefinition.MongoInstance;
-            string hashedPassword = data.Password; // Todo wait for bcrypt to be added by other pr
-            var usernameFilter = MongoFilterGenerator.GetEqualsFilter("username", data.Username);
-            var passwordFilter = MongoFilterGenerator.GetEqualsFilter("password", hashedPassword);
-            var userFilter = MongoFilterGenerator.CombineFilters(usernameFilter, passwordFilter);
-
-            var userDocs = dbServce.GetFilteredUsers(userFilter);            
+                // Get all users with request's username
+                var dbServce = MongoDefinition.MongoInstance;
+                var usernameFilter = MongoFilterGenerator.GetEqualsFilter("username", data.Username);
+                userDocs = dbServce.GetFilteredUsers(usernameFilter);
+            } catch (Exception)
+            {
+                return new UnauthorizedResult();
+            }
+            
             if(userDocs == null || userDocs.Count == 0)
             {
                 return new UnauthorizedResult();
-            } else if(userDocs.Count > 1)
-            {
-                // This should never happen
-                return new UnauthorizedResult();
             }
 
-            var user = BsonSerializer.Deserialize<User>(userDocs[0]);
+            // Go through the users and find the one whose password hash matches the request's password
+            // Doesn't use filter for password as bcrypt hashing creates a unique salt on every hash.
+            User user = null;
+            for(var i = 0; i < userDocs.Count && user == null; i++)
+            {
+                var person = BsonSerializer.Deserialize<User>(userDocs[i]);
+                if(BCryptNet.Verify(password, person.Password))
+                {
+                    user = person;
+                }
+            }
+
             if(user == null)
             {
                 return new UnauthorizedResult();
             }
 
+            // Create a jwt token
             var authService = AuthenticationDefinition.AuthticationInstance;
             var token = authService.CreateToken(user);
             return new OkObjectResult(token);
