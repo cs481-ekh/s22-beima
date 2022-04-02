3e0da5348ff2d1096c4e1ba1371e665a3d46b330
AshlynAdamson <58532471+AshlynAdamson@users.noreply.github.com>
2022-04-02 17:46:15 -0600

    fixing method header

--- BEIMA.Client/src/services/AddUser.js
+++ BEIMA.Client/src/services/AddUser.js
@@ -5,7 +5,7 @@ const API_URL = process.env.REACT_APP_API_URL;
  * POSTs a JSON object to the API for storage in DB
  *
  * @param The user details to add to the DB
- * @return Error message or the inserted device user
+ * @return Error message or the inserted user ID
  */
 export default async function addUser(userDetails) {
   //performs the post and returns an error message or the inserted user ID
