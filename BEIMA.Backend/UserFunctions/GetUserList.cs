2c3521523496a3275e8e02fda8cc84b451f39b8a
roguedeleter <47615072+roguedeleter@users.noreply.github.com>
2022-04-02 14:12:21 -0600

    Implement GetUserList endpoint

---
+++ BEIMA.Backend/UserFunctions/GetUserList.cs
@@ -0,0 +1,42 @@
+﻿using BEIMA.Backend.MongoService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson.Serialization;
+using System.Collections.Generic;
+
+namespace BEIMA.Backend.UserFunctions
+{
+    /// <summary>
+    /// Handles a get request involving multiple buildlings.
+    /// </summary>
+    public static class GetUserList
+    {
+        /// <summary>
+        /// Handles a user GET list request.
+        /// </summary>
+        /// <param name="req">The http request.</param>
+        /// <param name="log">The logger to log to.</param>
+        /// <returns>An http response containing the user information.</returns>
+        [FunctionName("GetUserList")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "user-list")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a user list request.");
+
+            var mongo = MongoDefinition.MongoInstance;
+            var users = mongo.GetAllUsers();
+            var userObjList = new List<User>();
+            foreach (var user in users)
+            {
+                var userObj = BsonSerializer.Deserialize<User>(user);
+                userObjList.Add(userObj);
+            }
+
+            return new OkObjectResult(userObjList);
+        }
+    }
+}
