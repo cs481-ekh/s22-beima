77f8afbcbc2e9bc6538c9d309542b4ca0fc4d5e3
Kenny-Miller <kennymilarhol@gmail.com>
2022-04-12 10:46:28 -0600

    Added test to check a request with null year manufactured

--- BEIMA.Backend.Test/DeviceFunctions/AddDeviceTest.cs
+++ BEIMA.Backend.Test/DeviceFunctions/AddDeviceTest.cs
@@ -81,6 +81,65 @@ namespace BEIMA.Backend.Test.DeviceFunctions
         }
 
         [Test]
+        public async Task NoDevice_AddDevice_NullYearManufactured_ReturnsValidId()
+        {
+            // ARRANGE
+            var building = new Building(new ObjectId("111111111111111111111111"), null, null, null);
+            building.SetLastModified(DateTime.UtcNow, "Anonymous");
+            building.SetLocation(null, null);
+            var deviceType = new DeviceType(new ObjectId("12341234abcdabcd43214321"), null, null, null);
+            deviceType.SetLastModified(DateTime.UtcNow, "Anonymous");
+            deviceType.AddField("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "TestName1");
+            deviceType.AddField("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", "TestName2");
+
+            // Setup mock database client.
+            Mock<IMongoConnector> mockDb = new Mock<IMongoConnector>();
+            mockDb.Setup(mock => mock.GetBuilding(It.Is<ObjectId>(oid => oid.Equals(new ObjectId("111111111111111111111111")))))
+                  .Returns(building.GetBsonDocument())
+                  .Verifiable();
+            mockDb.Setup(mock => mock.GetDeviceType(It.Is<ObjectId>(oid => oid.Equals(new ObjectId("12341234abcdabcd43214321")))))
+                  .Returns(deviceType.GetBsonDocument())
+                  .Verifiable();
+            mockDb.Setup(mock => mock.InsertDevice(It.IsAny<BsonDocument>()))
+                  .Returns(ObjectId.GenerateNewId())
+                  .Verifiable();
+            MongoDefinition.MongoInstance = mockDb.Object;
+
+            Mock<IStorageProvider> mockStorage = new Mock<IStorageProvider>();
+            mockStorage.Setup(mock => mock.PutFile(It.IsAny<IFormFile>()))
+                .Returns(Task.FromResult(Guid.NewGuid().ToString() + ".txt"))
+                .Verifiable();
+
+            StorageDefinition.StorageInstance = mockStorage.Object;
+
+            // Create request
+            var data = TestData._testAddDeviceNullYearManufactured;
+            var fileCollection = new FormFileCollection();
+
+            string? deviceId;
+            using (var fileStream = new ByteArrayContent(TestData._fileBytes).ReadAsStream())
+            {
+                fileCollection.Add(new FormFile(fileStream, 0, fileStream.Length, "files", "file.txt"));
+
+                var request = CreateMultiPartHttpRequest(data, fileCollection);
+                var logger = (new LoggerFactory()).CreateLogger("Testing");
+
+
+                // ACT
+                deviceId = ((ObjectResult)await AddDevice.Run(request, logger)).Value?.ToString();
+            }
+
+            // ASSERT
+            Assert.DoesNotThrow(() => mockDb.Verify(mock => mock.GetDeviceType(It.IsAny<ObjectId>()), Times.Once));
+            Assert.DoesNotThrow(() => mockDb.Verify(mock => mock.InsertDevice(It.IsAny<BsonDocument>()), Times.Once));
+
+            Assert.IsNotNull(deviceId);
+            Assert.DoesNotThrow(() => mockStorage.Verify(mock => mock.PutFile(It.IsAny<IFormFile>()), Times.Once));
+            Assert.DoesNotThrow(() => mockDb.Verify(mock => mock.InsertDevice(It.IsAny<BsonDocument>()), Times.Once));
+            Assert.That(ObjectId.TryParse(deviceId, out _), Is.True);
+        }
+
+        [Test]
         public async Task NoDevice_AddDeviceWithNoLocation_ReturnsValidId()
         {
             // ARRANGE
