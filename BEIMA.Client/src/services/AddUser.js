f2b01af07e926419791964d27855cf78794ade0b
AshlynAdamson <58532471+AshlynAdamson@users.noreply.github.com>
2022-04-12 16:23:40 -0600

    adding auto logout on 401 response

--- BEIMA.Client/src/services/AddUser.js
+++ BEIMA.Client/src/services/AddUser.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import {getCurrentUser} from './Authentication.js';
+import {getCurrentUser, logout} from './Authentication.js';
+import * as Constants from '../Constants.js';
 const API_URL = process.env.REACT_APP_API_URL;
 
 /**
@@ -17,6 +18,11 @@ export default async function addUser(userDetails) {
         return error.response;
     }
   });
+
+  if(dbCall.status === Constants.HTTP_UNAUTH_RESULT){
+    logout();
+    return;
+  }
 
   const response = {
     status: dbCall.status,
