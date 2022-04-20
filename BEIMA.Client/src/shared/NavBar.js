372ccec1fdb79950271c8c15ded69168a33173c9
Kenny-Miller <kennymilarhol@gmail.com>
2022-04-19 20:55:07 -0600

    Updated navbar styles

--- BEIMA.Client/src/shared/NavBar.js
+++ BEIMA.Client/src/shared/NavBar.js
@@ -1,59 +1,109 @@
 import logo from './BSU-logo.png';
-import { Row, Col, Container } from 'react-bootstrap';
 import { useState } from "react";
 import { getCurrentUser } from '../services/Authentication';
 import './shared.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { logout } from '../services/Authentication';
+import { MdDevices, MdLogout } from "react-icons/md";
+import { IoChevronForward, IoAddCircleOutline } from "react-icons/io5";
+import { CgTemplate } from "react-icons/cg"
+import { BiBuildings, BiHelpCircle } from "react-icons/bi"
+import { BsPinMap } from "react-icons/bs"
+import { HiOutlineUserGroup, HiOutlineInformationCircle } from "react-icons/hi"
 
 const NavBar = () => {
   const [currentUser] = useState(getCurrentUser);
 
   return (
     <div className="sharedNavBar">
-      <Container>
-        <Col>
-          <div className="logo"><img src={logo} className="bsu-logo" alt="bsu-logo" /></div>
-          <Row><div className="sharedHeader">BEIMA</div></Row>
-          <Row>
-            <Col className="pageLinks">
-              <Row className="pageLink">
-                <Link to="/devices" className="sharedText">Devices</Link>
-              </Row>
-              <Row className="pageLink">
-                <Link to="/deviceTypes" className="sharedText">Device Types</Link>
-              </Row>
-              <Row className="pageLink">
-                <Link to="/addDevice" className="sharedText">+ Add Device</Link>
-              </Row>
-              <Row className="pageLink">
-                <Link to="/addDeviceType" className="sharedText">+ Add Device Type</Link>
-              </Row>
-              <Row className="pageLink">
-                <Link to="/buildings" className="sharedText">Buildings</Link>
-              </Row>
-              <Row className="pageLink">
-                <Link to="/map" className="sharedText">Map</Link>
-              </Row>
-              {currentUser.Role === 'admin' ?
-              <Row className="pageLink">
-                <Link to="/users" className="sharedText">Users</Link>
-              </Row>
-              :<></>}
-              <Row className="pageLink">
-                <Link to="help" className="sharedText">? Help</Link>
-              </Row>
-              <Row className="pageLink">
-                <Link to="about" className="sharedText">About</Link>
-              </Row>
-              <Row className="pageLink">
-                <div className="sharedText logout" onClick={() => logout()}>Logout</div>
-              </Row>
-            </Col>
-          </Row>
-        </Col>
-      </Container>
+      <div className="logo"><img src={logo} className="bsu-logo" alt="Boise State University" /></div>
+      <div className="navbarDivider">BEIMA</div>
+      <Link to="/devices" className="sharedText">
+        <div className="textIcon">
+          <MdDevices/>
+          <div>Devices</div>
+        </div>
+        <IoChevronForward/>        
+      </Link>
 
+      <Link to="/deviceTypes" className="sharedText">
+        <div className="textIcon">
+          <CgTemplate/>
+          <div>Device Types</div>
+        </div>
+        <IoChevronForward/>     
+      </Link>
+
+      <Link to="/addDevice" className="sharedText">
+        <div className="textIcon">
+          <IoAddCircleOutline/>
+          <div>Add Device</div>
+        </div>
+        <IoChevronForward/>   
+      </Link>
+
+      <Link to="/addDeviceType" className="sharedText">
+        <div className="textIcon">
+          <IoAddCircleOutline/>
+          <div>Add Device Type</div>
+        </div>
+        <IoChevronForward/>         
+      </Link>
+
+      <Link to="/buildings" className="sharedText">
+        <div className="textIcon">
+          <BiBuildings/>
+          <div>Buildings</div>
+        </div>
+        <IoChevronForward/>  
+      </Link>
+
+      <Link to="/map" className="sharedText">
+        <div className="textIcon">
+          <BsPinMap/>
+          <div>Map</div>
+        </div>
+        <IoChevronForward/>  
+      </Link>
+      
+      {currentUser.Role === 'admin' ?
+        <>
+          <div className="navbarDivider">Admin</div>
+          <Link to="/users" className="sharedText">
+            <div className="textIcon">
+              <HiOutlineUserGroup/>
+              <div>Users</div>
+            </div>
+            <IoChevronForward/>  
+          </Link>
+        </>
+        
+      :<></>}
+
+      <div className="navbarDivider">Resources</div>
+      <Link to="help" className="sharedText">
+        <div className="textIcon">
+          <BiHelpCircle/>
+          <div>Help</div>
+        </div>
+        <IoChevronForward/>  
+      </Link>
+
+      <Link to="about" className="sharedText">
+        <div className="textIcon">
+          <HiOutlineInformationCircle/>
+          <div>About</div>
+        </div>
+        <IoChevronForward/>  
+      </Link>
+
+      <div className="sharedText logout" onClick={() => logout()}>
+        <div className="textIcon">
+          <MdLogout/>
+          <div>Logout</div>
+        </div>
+        <IoChevronForward/>  
+      </div>
     </div>
   );
 }
